a48966aa48a659b314cf3096f9bf6e05bf6dc748
luannth <luannth>
2025-04-03 17:08:19 +0700

    mod flex ui

--- client/src/components/ui/sidebar.tsx
+++ client/src/components/ui/sidebar.tsx
@@ -148,7 +148,7 @@ const SidebarProvider = React.forwardRef<
               } as React.CSSProperties
             }
             className={cn(
-              "group/sidebar-wrapper flex min-h-svh w-full text-sidebar-foreground has-[[data-variant=inset]]:bg-sidebar",
+              "group/sidebar-wrapper min-h-svh w-full text-sidebar-foreground has-[[data-variant=inset]]:bg-sidebar",
               className
             )}
             ref={ref}
