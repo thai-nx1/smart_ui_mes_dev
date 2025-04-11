9006c6703bdff444bb425c2d6a0a8786e15fb450
Oxiicore <40744770-Oxiicore@users.noreply.replit.com>
2025-04-11 06:51:47 +0000

    Restored to '35dd2571fad14b79efe666ae90496ca8686bcff5'
    Replit-Restored-To: 35dd2571fad14b79efe666ae90496ca8686bcff5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,6 @@ import Home from "@/pages/home";
 import FormsPage from "@/pages/forms";
 import WorkflowPage from "@/pages/workflow";
 import SubmissionPage from "@/pages/submission";
-import SubmissionCreatePage from "@/pages/submission-create";
 import DesignExamplePage from "@/pages/design-example";
 import RecordDetailPage from "@/pages/record-detail";
 import NotFound from "@/pages/not-found";
@@ -24,7 +23,6 @@ function Router() {
       <Route path="/menu/:menuId" component={WorkflowPage} />
       <Route path="/menu/:menuId/submenu/:subMenuId" component={WorkflowPage} />
       <Route path="/submission/:workflowId" component={SubmissionPage} />
-      <Route path="/submission/:workflowId/create" component={SubmissionCreatePage} />
       <Route path="/record/:menuId/:recordId" component={RecordDetailPage} />
       <Route path="/record/:menuId/:recordId/workflow/:workflowId" component={RecordDetailPage} />
       <Route path="/design" component={DesignExamplePage} />
